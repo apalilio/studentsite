69bec66b3a6abd5148ba38af184560989b29a683
apalilio <apalilio@hawaii.edu>
2024-05-01 13:14:02 -1000

    organized file

---
+++ art128/portfolio-v2/js/scriptinit.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+    // autoplay: {
+    //     delay: 2000,
+    // },
+    speed: 5000,
+    simulateTouch: true,
+    // autoplay: {
+    //     delay: 2000,
+    //     stopOnLastSlide: false,
+    //     disableOnInteraction: false
+    // },
+
+});
